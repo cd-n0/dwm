c600762d31c281f7e41fddea8c4ba59e6a73f30a
CD-N0 <cdn0@soydev.xyz>
2022-04-18 05:36:33 +0300

    autorun dwmblocks

--- dwm.c
+++ dwm.c
@@ -230,6 +230,7 @@ static void resizeclient(Client *c, int x, int y, int w, int h);
 static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
 static void run(void);
+static void runAutostart(void);
 static void scan(void);
 static int sendevent(Client *c, Atom proto);
 static void sendmon(Client *c, Monitor *m);
@@ -1568,6 +1569,11 @@ run(void)
 }
 
 void
+runAutostart(void) {
+	system("export STATUSBAR=\"dwmblocks\" ; killall dwmblocks ; dwmblocks &");
+}
+
+void
 scan(void)
 {
 	unsigned int i, num;
@@ -2593,6 +2599,7 @@ main(int argc, char *argv[])
 		die("pledge");
 #endif /* __OpenBSD__ */
 	scan();
+	runAutostart();
 	run();
 	if(restart) execvp(argv[0], argv);
 	cleanup();

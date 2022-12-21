457e8ee03f7d555bf0cfed6f019cb5b4963f3ba4
cdn0 <59338209+cd-n0@users.noreply.github.com>
2022-12-22 00:07:19 +0300

    changed window rules

--- config.def.h
+++ config.def.h
@@ -40,8 +40,8 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "Brave",   NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "Krita",   NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Ghidra",  NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "Steam", "Steam", "Steam",           1 << 7,    0,          0,           1,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */

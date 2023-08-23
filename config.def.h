216954a99798478736ac0a74f9a9a911fae1b5cc
cdn0 <59338209+cd-n0@users.noreply.github.com>
2023-08-23 23:07:51 +0300

    OBS - isfloating set to 0

--- config.def.h
+++ config.def.h
@@ -45,7 +45,7 @@ static const Rule rules[] = {
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "Steam",   NULL,     NULL,           1 << 7,    0,          0,           1,        0 },
 	{ "steam",   NULL,     NULL,           1 << 7,    0,          0,           1,        0 },
-	{ "obs",     NULL,     NULL,           0,         1,          0,           0,        1 },
+	{ "obs",     NULL,     NULL,           0,         0,          0,           0,        1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 

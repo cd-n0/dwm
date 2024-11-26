18acb344febdb0714a2911204f5e2a2f88b1ace1
cdn0 <59338209+cd-n0@users.noreply.github.com>
2024-11-26 16:48:33 +0300

    Vanitygaps patch

--- config.def.h
+++ config.def.h
@@ -3,6 +3,11 @@
 /* appearance */
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "mono:size=10", "JoyPixels:size=8" };
@@ -37,11 +42,26 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#include "vanitygaps.c"
+
 static const Layout layouts[] = {
     /* symbol     arrange function */
     { "[]=",      tile },    /* first entry is default */
-    { "><>",      NULL },    /* no layout function means floating behavior */
+    { "TTT",      bstack },
+    { "H[]",      deck },
     { "[M]",      monocle },
+    { "HHH",      grid },
+    { "###",      nrowgrid },
+    { "===",      bstackhoriz },
+    { "---",      horizgrid },
+    { "[@]",      spiral },
+    { "[\\]",     dwindle },
+    { ":::",      gaplessgrid },
+    { "|M|",      centeredmaster },
+    { ">M>",      centeredfloatingmaster },
+    { "><>",      NULL },    /* no layout function means floating behavior */
+    { NULL,       NULL },
 };
 
 /* key definitions */
@@ -75,9 +95,6 @@ static const Key keys[] = {
     { MODKEY,                       XK_space,           zoom,           {0} },
     { MODKEY,                       XK_Tab,             view,           {0} },
     { MODKEY,                       XK_q,               killclient,     {0} },
-    { MODKEY,                       XK_t,               setlayout,      {.v = &layouts[0]} },
-    { MODKEY|ShiftMask,             XK_t,               setlayout,      {.v = &layouts[1]} },
-    { MODKEY,                       XK_y,               setlayout,      {.v = &layouts[2]} },
     { MODKEY|ControlMask,           XK_space,           setlayout,      {0} },
     { MODKEY|ShiftMask,             XK_space,           togglefloating, {0} },
     { MODKEY,                       XK_0,               view,           {.ui = ~0 } },
@@ -95,6 +112,20 @@ static const Key keys[] = {
     TAGKEYS(                        XK_7,                               6)
     TAGKEYS(                        XK_8,                               7)
     TAGKEYS(                        XK_9,                               8)
+    /* Vanity gaps */
+    { MODKEY,                       XK_t,               setlayout,      {.v = &layouts[0]} },
+    { MODKEY|ShiftMask,             XK_t,               setlayout,      {.v = &layouts[1]} },
+    { MODKEY,                       XK_y,               setlayout,      {.v = &layouts[2]} },
+    { MODKEY|ShiftMask,             XK_y,               setlayout,      {.v = &layouts[3]} },
+    { MODKEY,                       XK_u,               setlayout,      {.v = &layouts[4]} },
+    { MODKEY|ShiftMask,             XK_u,               setlayout,      {.v = &layouts[5]} },
+    { MODKEY,                       XK_i,               setlayout,      {.v = &layouts[6]} },
+    { MODKEY|ShiftMask,             XK_i,               setlayout,      {.v = &layouts[7]} },
+    { MODKEY,                       XK_a,               togglegaps,     {0} },
+    { MODKEY|ShiftMask,             XK_a,               defaultgaps,    {0} },
+    { MODKEY,                       XK_z,               incrgaps,       {.i = +3 } },
+    { MODKEY,                       XK_x,               incrgaps,       {.i = -3 } },
+
     /* Application keys */
     { MODKEY,                       XK_w,               spawn,          SHCMD("$BROWSER")},
     { MODKEY,                       XK_KP_Add,          spawn,          SHCMD("boomer")},
@@ -133,4 +164,3 @@ static const Button buttons[] = {
     { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
     { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-

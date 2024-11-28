23f76188119a541e2e12dc87d23bc97fff4dd808
cdn0 <59338209+cd-n0@users.noreply.github.com>
2024-11-28 23:44:29 +0300

    xrdb patch

--- config.def.h
+++ config.def.h
@@ -12,15 +12,16 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "mono:size=10", "JoyPixels:size=8" };
 static const char dmenufont[]       = "mono:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-    /*               fg         bg         border   */
-    [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-    [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
@@ -84,7 +85,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 #define TERMINAL "st"
 
@@ -138,11 +139,25 @@ static const Key keys[] = {
 
     /* Application keys */
     { MODKEY,                       XK_w,               spawn,          SHCMD("$BROWSER")},
+    { MODKEY|ShiftMask,             XK_w,               spawn,          SHCMD(TERMINAL " -e nmtui")},
     { MODKEY,                       XK_KP_Add,          spawn,          SHCMD("boomer")},
     { MODKEY|ShiftMask,             XK_q,               spawn,          SHCMD("sysact") },
     { ShiftMask,                    XK_Pause,           spawn,          SHCMD("remaps") },
     { MODKEY|ShiftMask,             XK_d,               spawn,          SHCMD("passmenu") },
     { MODKEY,                       XK_grave,           spawn,          SHCMD("dmenuunicode") },
+    /* Function keys */
+    { MODKEY,                       XK_F1,              spawn,          SHCMD("dmenumount") },
+    { MODKEY,                       XK_F2,              spawn,          SHCMD("dmenuumount") },
+    { MODKEY,                       XK_F3,              spawn,          SHCMD("displayselect") },
+    { MODKEY,                       XK_F4,              spawn,          SHCMD(TERMINAL " -e pulsemixer") },
+    { MODKEY,                       XK_F5,              xrdb,           {.v = NULL } },
+    { MODKEY,                       XK_F6,              spawn,          SHCMD("") },
+    /* { MODKEY,                       XK_F7,              spawn,          SHCMD("") }, */
+    /* { MODKEY,                       XK_F8,              spawn,          SHCMD("") }, */
+    /* { MODKEY,                       XK_F9,              spawn,          SHCMD("") }, */
+    /* { MODKEY,                       XK_F10,             spawn,          SHCMD("") }, */
+    { MODKEY,                       XK_F11,             spawn,          SHCMD("mpv --untimed --no-cache --no-osc --no-input-default-bindings --profile=low-latency --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)") },
+    /* { MODKEY,                       XK_F12,             spawn,          SHCMD("") }, */
     /* music player */
     { MODKEY,                       XK_m,               spawn,          SHCMD(TERMINAL " -e ncmpcpp-ueberzug") },
     { MODKEY|ShiftMask,             XK_m,               spawn,          SHCMD("pamixer -t") },
